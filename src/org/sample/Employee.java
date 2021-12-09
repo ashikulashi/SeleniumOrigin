1047225803c9c2f9d72e24c4e1e5c696927ef266
balabv321 <balabv321@gmail.com>
2021-12-09 15:21:48 +0530

    ARif done his work

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -2,6 +2,13 @@ package org.sample;
 
 public class Employee {
 	
+	private void add() {
+		System.out.println("add");
+
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		
 		System.out.println("GitClass");
