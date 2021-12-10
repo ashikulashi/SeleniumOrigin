10c2acebb219fa7b40bc54b98bae9706ed24d064
balabv321 <balabv321@gmail.com>
2021-12-10 15:18:51 +0530

    Ashik written a code

--- src/org/sample/Employee.java
+++ src/org/sample/Employee.java
@@ -2,6 +2,10 @@ package org.sample;
 
 public class Employee {
 	
+	private void add2() {
+		System.out.println("add2");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("GitClass");
